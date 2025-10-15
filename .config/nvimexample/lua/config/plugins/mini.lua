31e38052e047ab1d8b17b78fa7c5131d31807da2
Sonny Kjellberg <sonnykjellberg@gmail.com>
2025-10-15 20:15:44 +0200

    minijump

--- .config/nvimexample/lua/config/plugins/mini.lua
+++ .config/nvimexample/lua/config/plugins/mini.lua
@@ -5,6 +5,7 @@ return {
       require("mini.surround").setup()
       require("mini.pairs").setup()
       require("mini.sessions").setup()
+      require("mini.jump").setup()
       require("mini.cursorword").setup({
         delay = 0,
       })
